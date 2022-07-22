2eb39318424155d7b21b0c22e8f8d42ed2e906b6
spellcry <sangiothebest@hotmail.com>
2022-07-22 14:54:27 +0200

    add griglia + celle colorate

--- bonus/js/scripts.js
+++ bonus/js/scripts.js
@@ -2,16 +2,20 @@ const grigliaElement = document.querySelector('.griglia');
 
 for(let i = 0; i<100; i++){
     const cellaElement = document.createElement('div');
+    cellaElement.classList.add('cella');
     const number = i + 1;
     let message = "";
     if ( number % 3 === 0 ) {
         message += 'Fizz';
+        cellaElement.classList.add('multiplo-3');
     }
     if ( number % 5 === 0 ) {
         message += 'Buzz';
+        cellaElement.classList.add('multiplo-5');
     }
     if ( ( number % 3 !=0 ) && ( number % 5 != 0 ) ) {
         message = number;
     }
-    console.log(message);
+    cellaElement.append(message);
+    grigliaElement.append(cellaElement);
 }
